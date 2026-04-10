3df307a927c6493cf604ba272a49b4eeed07cf68
alokranjanpogo <alokranjan18april@gmail.com>
2026-04-10 12:07:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from streamlit_autorefresh import st_autorefresh
 # ===============================
 # AUTO REFRESH
 # ===============================
-st_autorefresh(interval=90000, key="scada_refresh")
+st_autorefresh(interval=19000, key="scada_refresh")
 st.set_page_config(page_title="WTP Moharda SCADA", layout="wide")
 
 import datetime
