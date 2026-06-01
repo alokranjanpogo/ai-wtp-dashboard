6a449b772315a5df479a560827817917c221297f
alokranjanpogo <alokranjan18april@gmail.com>
2026-06-01 10:29:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -452,8 +452,12 @@ if mode == "🟢 Real-Time Data":
     # ======================================
     # NEW LIVE VALUES
     # ======================================
+import pytz
+from datetime import datetime
 
-    current_time_live = datetime.now().strftime("%H:%M:%S")
+    current_time_live = datetime.now(
+        pytz.timezone("Asia/Kolkata")
+    ).strftime("%H:%M:%S")
 
     new_inlet = round(
         random.uniform(35, 120),
