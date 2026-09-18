62f6d114f83a2a1485cbffc8dc56dca0d242dca3
alokranjanpogo <alokranjan18april@gmail.com>
2026-09-18 09:13:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5212,7 +5212,19 @@ st.markdown("""
 # TITLE
 # ==========================
 
-st.title("💧 Clariflocculator SCADA")
+st.markdown("""
+<div style="
+background:#F4F8FF;
+border-left:8px solid #0A2E6B;
+padding:15px;
+border-radius:8px;
+font-size:24px;
+font-weight:bold;
+color:#0A2E6B;">
+💧 Clariflocculator Running Status
+</div>
+""", unsafe_allow_html=True)
+`
 
 # ==========================
 # SIDEBAR
