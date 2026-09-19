fd54bdc28051b23139b12775c240284ecb12c7c0
alokranjanpogo <alokranjan18april@gmail.com>
2026-09-19 17:40:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -326,24 +326,26 @@ if mode == "📁 Manual Data":
     # SAFE SLICER
     # ========================================
     if len(history_df) > 1:
-    
-        selected_time = st.select_slider(
+
+    today_rows = history_df[
+        history_df["DateTime"].dt.date == datetime.now().date()
+    ]
+
+    default_date = (
+        today_rows["DateTime"].iloc[-1]
+        if len(today_rows) > 0
+        else history_df["DateTime"].iloc[-1]
+    )
+
+    selected_time = st.select_slider(
         "Select Date",
         options=history_df["DateTime"],
-        value=history_df[
-            history_df["DateTime"].dt.date == datetime.now().date()
-        ]["DateTime"].iloc[-1]
-        if len(
-            history_df[
-                history_df["DateTime"].dt.date == datetime.now().date()
-            ]
-        ) > 0
-        else history_df["DateTime"].iloc[-1]
-            
-    else:
-    
-        selected_time = history_df["DateTime"].iloc[0]
-            
+        value=default_date
+    )
+
+else:
+
+    selected_time = history_df["DateTime"].iloc[0]
    
     # ========================================
     # FILTER ROW
