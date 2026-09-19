c7f50571364c60c46d7c16b607128f87d96a6f5f
alokranjanpogo <alokranjan18april@gmail.com>
2026-09-19 17:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -327,21 +327,21 @@ if mode == "📁 Manual Data":
     # ========================================
     if len(history_df) > 1:
 
-    today_rows = history_df[
-        history_df["DateTime"].dt.date == datetime.now().date()
-    ]
-
-    default_date = (
-        today_rows["DateTime"].iloc[-1]
-        if len(today_rows) > 0
-        else history_df["DateTime"].iloc[-1]
-    )
-
-    selected_time = st.select_slider(
-        "Select Date",
-        options=history_df["DateTime"],
-        value=default_date
-    )
+        today_rows = history_df[
+            history_df["DateTime"].dt.date == datetime.now().date()
+        ]
+    
+        default_date = (
+            today_rows["DateTime"].iloc[-1]
+            if len(today_rows) > 0
+            else history_df["DateTime"].iloc[-1]
+        )
+    
+        selected_time = st.select_slider(
+            "Select Date",
+            options=history_df["DateTime"],
+            value=default_date
+        )
 
 else:
 
